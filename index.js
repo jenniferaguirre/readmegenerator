d5ca0fe4c7a5cdfc73bcb893c5b7dd89722c70e9
Jennifer Aguirre <jennifermichelle.aguirre@gmail.com>
2020-10-07 20:16:51 -0400

    statrt

---
+++ index.js
@@ -0,0 +1,22 @@
+var inquirer=require("inquirer");
+var fs =require ("fs")
+
+
+var questions=[
+  {
+type: "input",
+name:"github",
+message: " What is your Github username ? "}]
+
+console.log(questions)
+
+  
+
+
+
+
+   
+    
+
+
+
