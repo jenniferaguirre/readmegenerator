cd7aaee90ebf8a77ec8c69b8f827ecf5401fdfcd
Jennifer Aguirre <jennifermichelle.aguirre@gmail.com>
2020-10-07 20:21:23 -0400

    questionsshowing

--- index.js
+++ index.js
@@ -6,7 +6,40 @@ var questions=[
   {
 type: "input",
 name:"github",
-message: " What is your Github username ? "}]
+message: " What is your Github username ? "
+  },
+
+{type: "input",
+name:"projectName",
+message: " What is your Project Name ? "
+},
+
+
+
+{
+  type: "input",
+  name:"description",
+  message: " What is the Project about ? "
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
 
 console.log(questions)
 
